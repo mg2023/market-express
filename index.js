866cd16eeab608b92886fdd772b99fb67c0848c5
mg <amg.vergara@gmail.com>
2023-06-10 22:02:47 -0400

    Added products route

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const pool = new Pool({
   port: 5432,
 });
 
+app.get('/products', (req, res) => {
+  const consulta = pool.query('SELECT * FROM products', (err, result) => {
+    if (err) {
+      console.error('Error executing query', err);
+      res.status(500).send('Error executing query');
+    } else {
+      //res.send(`PostgreSQL connected! Current time: ${result.rows[0].now}`);
+      console.log('entra aca')
+      console.log(result.rows)
+      res.status(200).json(result.rows);
+    }
+  });
+});
+
 app.get('/', (req, res) => {
   pool.query('SELECT NOW()', (err, result) => {
     if (err) {
