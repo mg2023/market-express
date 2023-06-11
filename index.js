9d3b4c9523c94253a9205d86f61498ac40242899
mg <amg.vergara@gmail.com>
2023-06-11 01:06:01 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+  user: 'mg',
+  host: 'localhost',
+  database: 'market_db',
+  password: 'password',
+  port: 5432,
+});
+
+app.get('/', (req, res) => {
+  pool.query('SELECT NOW()', (err, result) => {
+    if (err) {
+      console.error('Error executing query', err);
+      res.status(500).send('Error executing query');
+    } else {
+      res.send(`PostgreSQL connected! Current time: ${result.rows[0].now}`);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
