e0c4da380c2be0ade5343a363e0152626e4836fb
mg <amg.vergara@gmail.com>
2023-06-14 20:34:31 -0400

    FIX  start script and add PORT env

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = require('./src/routes/routes')
+require('dotenv').config()
 
 const app = express();
-const port = 3000;
+const port = (process.env.PORT | 3000);
+
+console.log(process.env)
 
 const cors = require('cors')
 app.use(cors({ origin: "*", methods: ["GET", "POST"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true, preflightContinue: true }));
